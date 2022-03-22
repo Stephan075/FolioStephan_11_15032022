b9140ef75c361812b242954b364b0c4ba35b6e4d
Stephan <stephan.folioo@gmail.com>
2022-03-22 16:18:59 +0100

    Add components Collapsible JS/CSS not done

--- kasa/src/components/App/App.js
+++ kasa/src/components/App/App.js
@@ -16,7 +16,27 @@ function App() {
       {/* <HerroBanner /> */}
       {/* <ApartmentList /> */}
       {/* <Rating /> */}
-      <Collapsible />
+      <Collapsible label="Description">
+        {/* enfant */}
+        <p>
+          Venez découvrir Paris en séjournant dans ce nid douillet au coeur du
+          11ème. La vue sur le parc résidentiel saura vous reposer de vos
+          longues journées de tourisme dans la capitale française. Et pour les
+          plus fêtards d'entre vous, la rue Jean Pierre Timbaud vous permettra
+          de découvrir la night-life parisienne à seulement 5 minutes de
+          l'appartement.
+        </p>
+      </Collapsible>
+      <Collapsible label="Équipements">
+        <ul>
+          <li>Parking</li>
+          <li>Sèche Cheveux</li>
+          <li>Machine à laver</li>
+          <li>Wi-fi</li>
+          <li>Cuisine équipée</li>
+          <li>Télévision</li>
+        </ul>
+      </Collapsible>
     </>
   );
 }
--- kasa/src/components/collapsible/Collapsible.js
+++ kasa/src/components/collapsible/Collapsible.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./collapsible.css";
 
-const Collapsible = () => {
+const Collapsible = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="collapsible">
-      <button className="toggle">Toggle</button>
-      <div className="content">Content</div>
+      <button className="toggle" onClick={() => setIsOpen(!isOpen)}>
+        <span
+          className={isOpen ? "filter__custom-arrow" : "filter__custom-arrow"}
+        ></span>
+        {props.label}
+      </button>
+      {isOpen && <div className="content">{props.children}</div>}
     </div>
   );
 };
